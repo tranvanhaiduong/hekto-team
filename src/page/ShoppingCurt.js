4afc0ed9b245f39907a8d9deaaaa3947ee78347f
Duong <tranvanhaiduong709@gmail.com>
2022-07-26 08:34:51 +0700

    update mixin3

--- src/page/ShoppingCurt.js
+++ src/page/ShoppingCurt.js
@@ -1,10 +1,5 @@
 import Navigation from "../homehekto/Navigation";
 import CurtListPr from "../homehekto/CurtListPr";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "../scss/productCurt.scss";
-import "../scss/CurtProduct.scss";
-import "../scss/Label.scss";
 import { Fragment } from "react";
 
 function ShoppingCurt() {
