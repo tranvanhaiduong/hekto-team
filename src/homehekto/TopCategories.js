c7acc9df518b3fc545895df7832b24e21e787c81
trung1051996 <trung.nguyen@snaptec.co>
2022-07-26 16:31:43 +0700

    update

--- src/homehekto/TopCategories.js
+++ src/homehekto/TopCategories.js
@@ -1,15 +1,18 @@
-function TopCategories({ image }) {
+import { useNavigate } from "react-router-dom";
+
+function TopCategories({ image,title,price,pid }) {
+  const navigate= useNavigate();
   return (
     <div className="content__top__mini__LCW">
       <div className="content__top__mini__LCW__chair">
         <img alt="" src={image}></img>
         <div className="content__top__mini__LCW__chair__btn ">
-          <button >View Shop</button>
+          <button onClick={()=>navigate(`/product/${pid}`)}>View Shop</button>
         </div>
       </div>
       <div className="content__top__mini__LCW__price">
-        <h3>Mini LCW Chair</h3>
-        <p>$56.00</p>
+        <h3>{title}</h3>
+        <p>${price}.00</p>
       </div>
     </div>
   );
